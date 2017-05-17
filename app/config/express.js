2c783c4385ddbb694573c0eb5937f88bad67ae38
Nontapat <astider@gmail.com>
2017-05-17 14:43:13 +0700

    test new cors setting

--- app/config/express.js
+++ app/config/express.js
@@ -14,6 +14,16 @@ let urlencodedParser = bodyParser.urlencoded({
 });
 let jsonParser = bodyParser.json();
 
+
+let allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "http://localhost:3000, https://dsmbot.herokuapp.com, https://messengerchatbot-f6775.firebaseapp.com");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type');
+
+    next();
+}
+
+/*
 module.exports = function(app, express) {
 
   app.set('views', './app/views');
@@ -43,7 +53,8 @@ module.exports = function(app, express) {
     next();
 
   })
-
+*/
+  app.use(allowCrossDomain)
   app.use(express.static('public'))
 
 
